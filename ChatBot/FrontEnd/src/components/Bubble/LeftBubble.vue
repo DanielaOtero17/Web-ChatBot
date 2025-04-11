42d5d4b9d1df407e73a1fc823964ecec29397aaf
Daniela <jessot@hotmail.com>
2025-04-10 22:27:13 -0500

    Chat infrastructure is already

--- ChatBot/FrontEnd/src/components/Bubble/LeftBubble.vue
+++ ChatBot/FrontEnd/src/components/Bubble/LeftBubble.vue
@@ -1,30 +1,80 @@
 <script setup>
 
-const {messaje} = defineProps({
+const {message, esRespuesta} = defineProps({
     message:{
         type: String,
         required: true,
+    },
+    esRespuesta:{
+        type:Boolean,
+        required: true,
+        default: false,
     }
 })
+
 </script>
 
 <template>
 
-<div class="burbuja">{{message}}</div>
+<div v-if="esRespuesta" class="itemBot">
+    <v-avatar image="src\assets\avatarBot.avif" size="40" />
+    <div  class="burbujaBot" >
+        {{message}} 
+    </div>
+</div>
+<div v-else class="itemUser">
+    
+    <div  class="burbujaUser" >
+        {{message}} 
+    </div>
+    <v-avatar image="src\assets\avatarUsuario.png" size="40" />
+</div>
+
 
 </template>
 
 <style scoped>
 
-.burbuja{
-   /* width: fit-content;*/
-    max-width: 300px;
-    height: fit-content;
+.burbujaBot, .burbujaUser{
+    width: fit-content;
+    max-width: 500px;
     
-    padding: 5px;
-    border-radius: 10px 10px 10px 0px;
+    min-height: 30px;
+    max-height: fit-content;
+    
+    padding: 10px;
+    
+    margin: 10px;
+    margin-top: 20px;
+}
 
+.itemUser, .itemBot{
+    display: flex;
+    align-items:center;
+}
+
+.itemBot{
+    margin-left: 10px;
+}
+
+.itemUser{
+ justify-content: right;
+ margin-right: 10px;
+}
+
+.burbujaBot{
     color: white;
-    background-color: cornflowerblue;
+    background-color: rgb(16, 135, 214);
+    border-radius: 10px 10px 10px 0px;
+}
+
+.burbujaUser{
+    color: rgb(10, 10, 10);
+    background-color: white;
+    border-radius: 10px 10px 0px 10px;
+    right: 5px;
+   /* position:absolute;*/
+ 
+    
 }
 </style>
--- ChatBot/FrontEnd/src/components/Chat/Chat.vue
+++ ChatBot/FrontEnd/src/components/Chat/Chat.vue
@@ -1,39 +1,78 @@
 <script setup>
 
 import {ref} from 'vue'
+import LeftBubble from '../Bubble/LeftBubble.vue'
+
+const messageText = {
+    mensaje: "Hola soy DanielaBot. Estoy aquí para ayudarte",
+    esRespuesta: true,    
+}
+
+const messagesList= ref([])
+
+const textoModel = ref('');
+
+messagesList.value.push(messageText)
+
+const message2Text = {
+    mensaje: "Este es otro mensaje de prueba",
+    esRespuesta: false,    
+}
 
 
-const text = ref('')
-  const messages = ref([])
+const onClick = () =>{
+    
+    var msg = document.getElementById("textoPregunta").value;
 
-  function onClick () {
-    messages.value.push(text.value)
-  }
+    textoModel.value = "";
 
+    const nuevoMensaje ={
+        mensaje: msg,
+        esRespuesta: false,
+    }
+
+    messagesList.value.push(nuevoMensaje)
+
+  };
 
 </script>
 
 <template>
-
 
     <v-container class="chatcontainer ">
         <v-container class="chatHead ">
             <v-avatar image="src\assets\avatarBot.avif" size="60" />
             <h1 class="chatTitle">Mini ChatBot</h1>
         </v-container>
+
+
+            <v-virtual-scroll
+            :items="messagesList"
+            height="450"
+            item-height="48"
+            >
+
+            <template class="conversation" v-slot:default="{item}">
+                <left-bubble :message=item.mensaje :es-respuesta=item.esRespuesta />
+            </template>
+
+
+            </v-virtual-scroll>
         
-    <v-container class="chatEnd">
-        <v-text-field placeholder="Haz una pregunta ..." variant="regular" class="entryMessage">
-            
-        </v-text-field>
-        <v-btn elevation="0" color="#FAFAFA" height="60" append-icon="mdi-send"></v-btn>
 
-    </v-container>
+        <v-container class="chatEnd">
+            <v-text-field id="textoPregunta" placeholder="Haz una pregunta ..." variant="regular" 
+            class="entryMessage" 
+            name="textoPregunta"
+            v-model:model-value="textoModel"
+            >
+                
+            </v-text-field>
+            <v-btn elevation="0" color="#FAFAFA" height="60" append-icon="mdi-send" @Click="onClick"></v-btn>
+
+        </v-container>
         
-            
     </v-container>
-
-
 
 </template>
 
@@ -44,13 +83,19 @@ const text = ref('')
     background-color: hsl(0, 0%, 96%);
     border-radius: 10px;
     padding: 0px;
-    width: 800px;
-    height: 500px;
+    width: 700px;
+    height: 600px;
 }
+
+.conversation{
+    margin: 10px;
+    padding: 10px;
+}
+
 
 .chatHead{
 
-    background-color: rgb(16, 135, 214);
+    background-color: rgb(16, 17, 17);
     border-top-left-radius: 10px;
     border-top-right-radius: 10px;
 
@@ -64,7 +109,6 @@ const text = ref('')
     padding: 0px;
     display: flex;
     align-items: e;
-    margin-top: 355px;
 }
 
 .chatTitle{
@@ -80,13 +124,14 @@ const text = ref('')
     border-bottom-left-radius: 20px;
 
     height: 60px;
-
     
 }
 
-v-btn{
-    align-self: flex-end;
+.v-btn{
+    border-bottom-right-radius: 20px;
 }
+
+
 
 </style>
 
